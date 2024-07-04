d77a2aa5d3acfb96722442536ba2ee47db5d3d78
James Matherne <93159223+jmatherne65@users.noreply.github.com>
2024-07-04 02:35:56 -0500

    Began Tony Hawk Theming

--- zenith-interview-prep/src/components/Todolist.tsx
+++ zenith-interview-prep/src/components/Todolist.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { VStack, HStack, Input, Button, Checkbox, Heading } from '@chakra-ui/react';
+import { VStack, HStack, Input, Button, Checkbox, Heading, Text } from '@chakra-ui/react';
 
 interface Todo {
     id: number;
@@ -7,39 +7,61 @@ interface Todo {
     completed: boolean;
 }
 
-const Todolist: React.FC = () => {
-    const [Todos, setTodos] = useState<Todo[]>([]);
+const TodoList: React.FC = () => {
+    const [todos, setTodos] = useState<Todo[]>([]);
     const [text, setText] = useState('');
 
-    const addTodo = () =>{
-        if (text.trim()){
-            setTodos([...Todos, { id: Date.now(), text, completed: false }]);
+    const addTodo = () => {
+        if (text.trim()) {
+            setTodos([...todos, { id: Date.now(), text, completed: false }]);
             setText('');
         }
     };
 
     const toggleTodo = (id: number) => {
-        setTodos(Todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo));
+        setTodos(todos.map(todo => 
+            todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        ));
     };
 
     const deleteTodo = (id: number) => {
-        setTodos(Todos.filter(todo => todo.id !== id));
-      };
+        setTodos(todos.filter(todo => todo.id !== id));
+    };
 
     return (
         <VStack spacing={4}>
-          <Heading as="h1" size="lg">To-Do List</Heading>
-          <HStack>
-            <Input value={text} onChange={(e) => setText(e.target.value)} placeholder="Add a new task" />
-            <Button onClick={addTodo} colorScheme="blue">Add</Button>
-          </HStack>
-          {Todos.map(todo => (
-            <HStack key={todo.id} spacing={4}>
-              <Checkbox isChecked={todo.completed} onChange={() => toggleTodo(todo.id)}>{todo.text}</Checkbox>
+            <Heading as="h1" size="lg">TONY HAWK'S</Heading>
+            <HStack>
+                <Input 
+                    value={text} 
+                    onChange={(e) => setText(e.target.value)} 
+                    placeholder="Add a new task" 
+                />
+                <Button onClick={addTodo} colorScheme="blue">Add</Button>
             </HStack>
-          ))}
+            {todos.length === 0 ? (
+                <Text>No tasks added yet.</Text>
+            ) : (
+                todos.map(todo => (
+                    <HStack key={todo.id} spacing={4}>
+                        <Checkbox 
+                            isChecked={todo.completed} 
+                            onChange={() => toggleTodo(todo.id)}
+                        >
+                            {todo.text}
+                        </Checkbox>
+                        <Button 
+                            size="sm" 
+                            colorScheme="red" 
+                            onClick={() => deleteTodo(todo.id)}
+                        >
+                            Delete
+                        </Button>
+                    </HStack>
+                ))
+            )}
         </VStack>
-      );
-    };
-    
-    export default Todolist;
+    );
+};
+
+export default TodoList;
